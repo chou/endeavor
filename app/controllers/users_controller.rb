e88c6c5ccd7fa5af5f8cab7b2a7b69ff31c3f0a7
Julia Chou <juliac@womply.com>
2015-05-06 10:48:45 -0700

    Add a root index page to view all users and add create user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,17 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
 end
